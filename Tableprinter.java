2fea492851d9363c77b41707dd234447efcaca72
Diwakar Soma <73683780+somadiwakar@users.noreply.github.com>
2022-05-16 09:24:13 +0530

    Create Tableprinter.java

---
+++ Tableprinter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+      Scanner SC = new Scanner(System.in);
+      int n = SC.nextInt();
+      for (int i=1;i<=10;i++){
+            System.out.print(n);
+             System.out.print("x");
+              System.out.print(i);
+               System.out.print("=");
+                System.out.println(n*i);
+      }
+    }
+}
